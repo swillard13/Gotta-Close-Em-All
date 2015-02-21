2acbf0a96799df109baf7cda3bed50bc428ed5ed
Rochelle <rwillard@usc.edu>
2015-02-21 15:16:13 -0800

    fix clicking issue

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-$('[name="comment_and_close"]').click(function(event){
+console.log('hellelekfjsadkljf;a');
+$('button[name="comment_and_close"]').click(function(event){
     catchPokemon();
-}
+});
 
 function catchPokemon() {
 	console.log('Caught a POKEMON');
